a6438790504309e67c4efa1a4a5b606a60d7d138
Lucas Samuel <2022335871@ifam.edu.br>
2024-09-03 19:13:27 -0300

    oioioi

---
+++ app/src/main/java/com/example/healthgeneration/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.healthgeneration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void proximo2(View view){
+        Intent in = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(in);
+    }
+
+    public void proximo(View view){
+        Intent in = new Intent(LoginActivity.this, PrincipalActivity.class);
+        startActivity(in);
+    }
+
+
+
+}
