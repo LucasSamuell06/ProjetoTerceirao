e4a2b2533043ec36aec2958103b8bf0674f008f9
Lucas Samuel <2022335871@ifam.edu.br>
2024-11-25 20:42:03 -0400

    oioioi

--- app/src/main/java/com/example/healthgeneration/NovoExercicio2Activity.java
+++ app/src/main/java/com/example/healthgeneration/NovoExercicio2Activity.java
@@ -3,14 +3,16 @@ package com.example.healthgeneration;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class NovoExercicio2Activity extends AppCompatActivity {
+
+    private CheckBox checkHiper, checkDefi, checkEma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,27 @@ public class NovoExercicio2Activity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_novo_exercicio2);
 
+        // Inicializando os elementos do layout
+        checkHiper = findViewById(R.id.checkHiper);
+        checkDefi = findViewById(R.id.checkDefi);
+        checkEma = findViewById(R.id.checkEma);
     }
 
-    public void next(View view){
-        Intent in = new Intent(NovoExercicio2Activity.this, NovoExercicio3Activity.class);
-        startActivity(in);
+    // Método chamado ao clicar no botão "Continuar"
+    public void next(View view) {
+        if (validarSelecoes()) {
+            // Navegar para a próxima tela se ao menos uma opção foi selecionada
+            Intent in = new Intent(NovoExercicio2Activity.this, NovoExercicio3Activity.class);
+            startActivity(in);
+        } else {
+            // Mostrar mensagem de erro se nenhuma opção foi selecionada
+            Toast.makeText(this, "Por favor, selecione pelo menos um objetivo!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Método para validar as seleções
+    private boolean validarSelecoes() {
+        // Retorna verdadeiro se pelo menos um dos checkboxes ou o radiobutton foi selecionado
+        return checkHiper.isChecked() || checkDefi.isChecked() || checkEma.isChecked();
     }
 }
--- app/src/main/java/com/example/healthgeneration/PrincipalActivity.java
+++ app/src/main/java/com/example/healthgeneration/PrincipalActivity.java
@@ -1,34 +1,60 @@
 package com.example.healthgeneration;
 
-import static com.example.healthgeneration.R.*;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
-import androidx.activity.EdgeToEdge;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import com.google.android.material.bottomnavigation.BottomNavigationView;;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class PrincipalActivity extends AppCompatActivity{
-
-
+public class PrincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_principal);
+        setContentView(R.layout.activity_principal);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Define o fragmento inicial
+        loadFragment(new MenuFragment());
+
+        // Listener para navegação no BottomNavigation
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                if (item.getItemId() == R.id.nav_menu) {
+                    selectedFragment = new MenuFragment();
+                } else if (item.getItemId() == R.id.nav_relatorio) {
+                    selectedFragment = new RelatorioFragment();
+                } else if (item.getItemId() == R.id.nav_perfil) {
+                    selectedFragment = new PerfilFragment();
+                }
+
+                if (selectedFragment != null) {
+                    loadFragment(selectedFragment);
+                }
+                return true;
+            }
+        });
     }
 
-    public void proximo4(View view){
-        Intent in = new Intent(PrincipalActivity.this, NovoExercicioActivity.class);
-        startActivity(in);
+    // Método para carregar fragments
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+    // Método para navegação via Intent
+    public void proximo4(View view) {
+        Intent intent = new Intent(PrincipalActivity.this, NovoExercicioActivity.class);
+        startActivity(intent);
     }
 }
